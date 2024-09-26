1ad734e0fd0735473b66018e1a7f143540f9fa3f
AMD <michojekunle1@gmail.com>
2024-09-26 16:22:31 +0100

    changes

--- challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -5,17 +5,30 @@ pragma solidity 0.8.4; //Do not change the solidity version as it negativly impa
 import "./YourToken.sol";
 
 contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+	YourToken public yourToken;
+	address owner;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+		owner = msg.sender;
+	}
+
+	// ToDo: create a payable buyTokens() function:
+	function buyTokens() external payable {}
+
+	// ToDo: create a withdraw() function that lets the owner withdraw ETH
+	function withdraw() external {
+		require(address(this).balance > 0, "Contrat balance is negligible");
+
+		payable(owner).transfer(address(this).balance);
+	}
+
+	// ToDo: create a sellTokens(uint256 _amount) function:
+  function sellTokens(uint256 _amount) external {
+    require(_amount > 0, 'zero amount not allowed');
+
+    
   }
-
-  // ToDo: create a payable buyTokens() function:
-
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-  // ToDo: create a sellTokens(uint256 _amount) function:
 }
--- challenge-2-token-vendor/packages/hardhat/contracts/YourToken.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/YourToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 contract YourToken is ERC20 {
-  constructor() ERC20("Gold", "GLD") {
-    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+  constructor() ERC20("Amd Token", "AMDT") {
+    _mint(0x573747e210a4C329FE2C418C2Bb629CD3e2c915b, 1000 * 10 ** 18);
   }
 }
